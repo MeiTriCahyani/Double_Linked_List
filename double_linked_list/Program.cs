52e213cbc255d1dd0717626635bc510604e37471
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-11-14 17:04:58 +0700

    class node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace double_linked_list
 {
-    internal class Program
+    class Node
     {
+        /*node class represents the noe of doubly linked list.
+        it consists of the information part and links to 
+        its succeeding and precceding
+        in terms of next and previous */
+
+        public int noMhs;
+        public string name;
+
+        //point to teh succeding note
+        public Node next;
+
+        //point to the precceding node
+        public Node prev;
+    }
+
+    
+
         static void Main(string[] args)
         {
         }
-    }
+    
 }
