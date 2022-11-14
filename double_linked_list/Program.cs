59bfd89b317a915f0aff370d63a862ada47ab981
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-11-14 17:24:24 +0700

    public bool search

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -91,6 +91,14 @@ namespace double_linked_list
             current.prev = newNode;
             previous.next = newNode;
         }
+        public bool search(int rollNo, ref Node previous,ref Node current)
+        {
+            for (previous = current = START; current != null &&
+                rollNo != current.noMhs; previous = current,
+                current = current.next) { }
+            return (current != null);
+        }
+         
     }
 
         static void Main(string[] args)
